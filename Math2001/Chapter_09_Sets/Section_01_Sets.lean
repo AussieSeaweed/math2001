604c7fffbe39c2b1c2762a686299fd58638e98ee
Juho Kim <juho-kim@outlook.com>
2025-07-01 03:36:20 -0400

    Complete 9.1

--- Math2001/Chapter_09_Sets/Section_01_Sets.lean
+++ Math2001/Chapter_09_Sets/Section_01_Sets.lean
@@ -129,10 +129,12 @@ example : 4 ∈ {a : ℚ | a < 3} := by
   sorry
 
 example : 4 ∉ {a : ℚ | a < 3} := by
-  sorry
+  dsimp
+  numbers
 
 example : 6 ∈ {n : ℕ | n ∣ 42} := by
-  sorry
+  use 7
+  numbers
 
 example : 6 ∉ {n : ℕ | n ∣ 42} := by
   sorry
@@ -142,24 +144,36 @@ example : 8 ∈ {k : ℤ | 5 ∣ k} := by
   sorry
 
 example : 8 ∉ {k : ℤ | 5 ∣ k} := by
-  sorry
+  apply Int.not_dvd_of_exists_lt_and_lt 8 5
+  use 1
+  constructor <;> numbers
 
 example : 11 ∈ {n : ℕ | Odd n} := by
-  sorry
+  use 5
+  numbers
 
 example : 11 ∉ {n : ℕ | Odd n} := by
   sorry
 
 
 example : -3 ∈ {x : ℝ | ∀ y : ℝ, x ≤ y ^ 2} := by
-  sorry
+  intro y
+  calc
+    -3
+      ≤ 0 := by numbers
+    _ ≤ y ^ 2 := by extra
 
 example : -3 ∉ {x : ℝ | ∀ y : ℝ, x ≤ y ^ 2} := by
   sorry
 
 
 example : {a : ℕ | 20 ∣ a} ⊆ {x : ℕ | 5 ∣ x} := by
-  sorry
+  rw [Set.subset_def]
+  dsimp
+  intro a ⟨k, h⟩
+  use 4 * k
+  rw [h]
+  ring
 
 example : {a : ℕ | 20 ∣ a} ⊈ {x : ℕ | 5 ∣ x} := by
   sorry
@@ -169,16 +183,42 @@ example : {a : ℕ | 5 ∣ a} ⊆ {x : ℕ | 20 ∣ x} := by
   sorry
 
 example : {a : ℕ | 5 ∣ a} ⊈ {x : ℕ | 20 ∣ x} := by
-  sorry
+  rw [Set.subset_def]
+  dsimp
+  push_neg
+  use 5
+  constructor
+  · use 1
+    numbers
+  · apply Nat.not_dvd_of_exists_lt_and_lt
+    use 0
+    constructor <;> numbers
 
 example : {r : ℤ | 3 ∣ r} ⊆ {s : ℤ | 0 ≤ s} := by
   sorry
 
 example : {r : ℤ | 3 ∣ r} ⊈ {s : ℤ | 0 ≤ s} := by
-  sorry
+  rw [Set.subset_def]
+  dsimp
+  push_neg
+  use -3
+  constructor
+  · use -1
+    numbers
+  · numbers
 
 example : {m : ℤ | m ≥ 10} ⊆ {n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n} := by
-  sorry
+  rw [Set.subset_def]
+  dsimp
+  intro m h
+  calc
+    m ^ 3 - 7 * m ^ 2
+      = m * m ^ 2 - 7 * m ^ 2 := by ring
+    _ ≥ 10 * m ^ 2 - 7 * m ^ 2 := by rel [h]
+    _ = 3 * m * m := by ring
+    _ ≥ 3 * 10 * m := by rel [h]
+    _ = 4 * m + 26 * m := by ring
+    _ ≥ 4 * m := by extra
 
 example : {m : ℤ | m ≥ 10} ⊈ {n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n} := by
   sorry
@@ -186,7 +226,21 @@ example : {m : ℤ | m ≥ 10} ⊈ {n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n} := by
 
 namespace Int
 example : {n : ℤ | Even n} = {a : ℤ | a ≡ 6 [ZMOD 2]} := by
-  sorry
+  ext n
+  dsimp
+  constructor
+  · intro h
+    obtain ⟨k, hk⟩ := h
+    use (k - 3)
+    rw [hk]
+    ring
+  · intro h
+    obtain ⟨k, hk⟩ := h
+    use (k + 3)
+    calc
+      n = (n - 6) + 6 := by ring
+      _ = 2 * k + 6 := by rw [hk]
+      _ = 2 * (k + 3) := by ring
 
 example : {n : ℤ | Even n} ≠ {a : ℤ | a ≡ 6 [ZMOD 2]} := by
   sorry
@@ -197,16 +251,43 @@ example : {t : ℝ | t ^ 2 - 5 * t + 4 = 0} = {4} := by
   sorry
 
 example : {t : ℝ | t ^ 2 - 5 * t + 4 = 0} ≠ {4} := by
-  sorry
+  ext
+  dsimp
+  push_neg
+  use 1
+  left
+  constructor <;> numbers
 
 example : {k : ℤ | 8 ∣ 6 * k} = {l : ℤ | 8 ∣ l} := by
   sorry
 
 example : {k : ℤ | 8 ∣ 6 * k} ≠ {l : ℤ | 8 ∣ l} := by
-  sorry
+  ext
+  dsimp
+  push_neg
+  use 4
+  left
+  constructor
+  · use 3
+    numbers
+  · apply Int.not_dvd_of_exists_lt_and_lt
+    use 0
+    constructor <;> numbers
 
 example : {k : ℤ | 7 ∣ 9 * k} = {l : ℤ | 7 ∣ l} := by
-  sorry
+  ext k
+  dsimp
+  constructor
+  · intro ⟨n, hn⟩
+    use 4 * n - 5 * k
+    calc
+      k = 4 * (9 * k) - 35 * k := by ring
+      _ = 4 * (7 * n) - 35 * k := by rw [hn]
+      _ = 7 * (4 * n - 5 * k) := by ring
+  · intro ⟨n, hn⟩
+    use 9 * n
+    rw [hn]
+    ring
 
 example : {k : ℤ | 7 ∣ 9 * k} ≠ {l : ℤ | 7 ∣ l} := by
   sorry
@@ -216,7 +297,34 @@ example : {1, 2, 3} = {1, 2} := by
   sorry
 
 example : {1, 2, 3} ≠ {1, 2} := by
-  sorry
+  ext
+  dsimp
+  push_neg
+  use 3
+  left
+  constructor
+  · right
+    right
+    numbers
+  · constructor <;> numbers
 
 example : {x : ℝ | x ^ 2 + 3 * x + 2 = 0} = {-1, -2} := by
-  sorry
+  ext x
+  dsimp
+  constructor
+  · intro h1
+    have h2 :=
+      calc
+        (x + 1) * (x + 2)
+          = x ^ 2 + 3 * x + 2 := by ring
+        _ = 0 := by rw [h1]
+    rw [mul_eq_zero] at h2
+    obtain h2 | h2 := h2
+    · left
+      addarith [h2]
+    · right
+      addarith [h2]
+  · intro h
+    obtain h | h := h <;>
+    · rw [h]
+      numbers
