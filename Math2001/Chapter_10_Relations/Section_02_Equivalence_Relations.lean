7b0e3789779edca311edca827745653c2aba6696
Juho Kim <juho-kim@outlook.com>
2025-07-02 09:47:05 -0400

    Complete 10.2

--- Math2001/Chapter_10_Relations/Section_02_Equivalence_Relations.lean
+++ Math2001/Chapter_10_Relations/Section_02_Equivalence_Relations.lean
@@ -168,13 +168,43 @@ section
 local infix:50 "∼" => fun (a b : ℤ) ↦ ∃ m n, m > 0 ∧ n > 0 ∧ a * m = b * n
 
 example : Reflexive (· ∼ ·) := by
-  sorry
+  intro
+  use 1, 1
+  constructor
+  · numbers
+  · constructor
+    · numbers
+    · ring
 
 example : Symmetric (· ∼ ·) := by
-  sorry
+  intro _ _ ⟨m, n, ⟨hm, hn, h⟩⟩
+  use n, m
+  constructor
+  · apply hn
+  · constructor
+    · apply hm
+    · rw [h]
 
 example : Transitive (· ∼ ·) := by
-  sorry
+  intro x y z ⟨a, b, ⟨ha, hb, hxy⟩⟩ ⟨c, d, ⟨hc, hd, hyz⟩⟩
+  use a * c, b * d
+  constructor
+  · calc
+      a * c
+        > 0 * 0 := by rel [ha, hc]
+      _ = 0 := by numbers
+  · constructor
+    · calc
+        b * d
+          > 0 * 0 := by rel [hb, hd]
+        _ = 0 := by numbers
+    · calc
+        x * (a * c)
+          = (x * a) * c := by ring
+        _ = (y * b) * c := by rw [hxy]
+        _ = (y * c) * b := by ring
+        _ = (z * d) * b := by rw [hyz]
+        _ = z * (b * d) := by ring
 
 end
 
@@ -183,13 +213,16 @@ section
 local infix:50 "∼" => fun ((a, b) : ℕ × ℕ) (c, d) ↦ a + d = b + c
 
 example : Reflexive (· ∼ ·) := by
-  sorry
+  intro
+  ring
 
 example : Symmetric (· ∼ ·) := by
-  sorry
+  intro _ _ h
+  addarith [h]
 
 example : Transitive (· ∼ ·) := by
-  sorry
+  intro _ _ _ h1 h2
+  addarith [h1, h2]
 
 end
 
@@ -199,12 +232,44 @@ local infix:50 "∼" => fun ((a, b) : ℤ × ℤ) (c, d) ↦
   ∃ m n, m > 0 ∧ n > 0 ∧ m * b * (b ^ 2 - 3 * a ^ 2) = n * d * (d ^ 2 - 3 * c ^ 2)
 
 example : Reflexive (· ∼ ·) := by
-  sorry
+  intro
+  use 1, 1
+  constructor
+  · numbers
+  · constructor
+    · numbers
+    · ring
 
 example : Symmetric (· ∼ ·) := by
-  sorry
+  intro _ _ ⟨m, n, ⟨hm, hn, h⟩⟩
+  dsimp at *
+  use n, m
+  constructor
+  · use hn
+  · constructor
+    · use hm
+    · rw [h]
 
 example : Transitive (· ∼ ·) := by
-  sorry
+  intro (a, b) (c, d) (e, f) ⟨x, y, ⟨hx, hy, h1⟩⟩ ⟨z, w, ⟨hz, hw, h2⟩⟩
+  dsimp at *
+  use x * z, y * w
+  constructor
+  · calc
+      x * z
+        > 0 * 0 := by rel [hx, hz]
+      _ = 0 := by numbers
+  · constructor
+    · calc
+        y * w
+          > 0 * 0 := by rel [hy, hw]
+        _ = 0 := by numbers
+    · calc
+        x * z * b * (b ^ 2 - 3 * a ^ 2)
+          = z * (x * b * (b ^ 2 - 3 * a ^ 2)) := by ring
+        _ = z * (y * d * (d ^ 2 - 3 * c ^ 2)) := by rw [h1]
+        _ = y * (z * d * (d ^ 2 - 3 * c ^ 2)) := by ring
+        _ = y * (w * f * (f ^ 2 - 3 * e ^ 2)) := by rw [h2]
+        _ = y * w * f * (f ^ 2 - 3 * e ^ 2) := by ring
 
 end
